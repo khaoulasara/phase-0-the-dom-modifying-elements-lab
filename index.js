d0bef49351365e8233ab8da31eac283a9410d706
vboxuser <khaoulaaourrath@gmail.com>
2023-09-12 11:46:06 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// Define a variable
+const greeting = "hi";
+
+// Remove the existing 'main' element
+const mainElement = document.querySelector("main#main");
+mainElement.remove();
+
+// Create a new 'h1' element and configure it
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
